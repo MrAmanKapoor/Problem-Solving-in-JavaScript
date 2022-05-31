4f86f85674ce0c052dade0a2314ef41c18b7e632
Aman Kapoor <amankapoor140@gmail.com>
2022-05-31 19:42:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -220,6 +220,20 @@ alert
 }
 
 
+var myOject = {
+    foo: "bar",
+    func: function() {
+        var self = this;
+        console.log("outer func:  this.foo = " + this.foo);
+        console.log("outer func:  self.foo = " + self.foo);
+        (function() {
+            console.log("inner func:  this.foo = " + this.foo);
+            console.log("inner func:  self.foo = " + self.foo);
+        }());
+    }
+};
+myObject.func();
+
 /*
 
 function Sum_of_numbers_within_an_array() {
